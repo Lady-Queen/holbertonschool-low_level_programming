4a72e8f359a5da74c5013437aaeb9489d3ea027a
ladyjohanaacosta@icloud.com <ladyjohanaacosta@icloud.com>
2023-06-23 15:46:28 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-  * int main 
-  * 
+  * main - 
+  * Return: 0;
   */
 int main(void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
