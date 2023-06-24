e3a26e8d71d53494c45a8b1aed3ad33e9ccee5bc
ladyjohanaacosta@icloud.com <ladyjohanaacosta@icloud.com>
2023-06-24 09:01:27 -0700

    git

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,14 +5,14 @@
 
 #include <stdio.h>
 
-/** 
- * main - printf:("with proper grammar, 
+/**
+ * main - printf:("with proper grammar,
  * but the outcome is a piece of art,
  * Return: 0;
  */
 
 int main(void)
-	
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
