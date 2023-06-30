5d2cbb5abffcd3157105b4c596a78b95477b9760
ladyjohanaacosta@icloud.com <ladyjohanaacosta@icloud.com>
2023-06-29 21:38:17 -0700

    6-print_numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0
- * Description: Prints all single digit numbers of base 10 starting from 0 
+ * main - Prints all single digit numbers of base 10 starting from 0
  * Return: (0);
  */
 
 int main(void)
 {
-	int i;
-		for (i = '0'; i < '10'; i++)
-		{	
-		printf ("%d", i);
-		}
-		printf ('\n')
-		}
-		return (0);
+	int d;
+
+	for (d = '0'; d < '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
 }
---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the lo main - Prints the loww
+ *
